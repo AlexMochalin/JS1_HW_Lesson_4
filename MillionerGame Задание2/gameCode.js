036de873b5f5c4a665b0afdd67c912311cb4eb56
Александр Мочалин <37539216+AlexMochalin@users.noreply.github.com>
2018-06-19 11:18:17 +0300

    Update gameCode.js
    Добавил переменную numberQuestion и оператор switch.

--- MillionerGame Задание2/gameCode.js
+++ MillionerGame Задание2/gameCode.js
@@ -4,14 +4,14 @@ var totalGain = 0;
 for (var i = 0; i < game.length; i++) {
 	do {//Выводим первый вопрос
 		ok = false;
-		event = +prompt(game[i].question + '\n' + 'Выберите правильный ответ и укажите его номер' + '\n' + game[i].answer1 + game[i].answer2 + game[i].answer3 + game[i].answer4 + '-1 - Выход из игры');
+		event = +prompt('Вопрос №' + i +'. ' + game[i].question + '\n' + 'Выберите правильный ответ и укажите его номер' + '\n' + game[i].answer1 + game[i].answer2 + game[i].answer3 + game[i].answer4 + '-1 - Выход из игры');
 		if (event == -1) {
 			break;
 		}
 		else {
 			ok = isAnswer(game[i].amountOfAnswers, event);
 		}
-} 	while (!ok);
+	} while (!ok);
 
 	if(event == game[i].correctAnswer) {
 		totalGain += game[i].gain;
@@ -28,7 +28,29 @@ for (var i = 0; i < game.length; i++) {
 
 if (totalGain != 0) {
 	alert('Вы очень умный, спасибо за игру!');	
-} 
+}
+var numberQuestion = +prompt('Введите номер вопроса, который хотите посмотреть или -1 для выхода');
+switch (numberQuestion){
+	case 1:
+		alert('game[0].question');
+		break;
+	case 2:
+		alert('game[1].question');
+		break;
+	case 3:
+		alert('game[2].question');
+		break;
+	case 4:
+		alert('game[3].question');
+		break;
+	case 5:
+		alert('game[4].question');
+		break;
+	case -1:
+		break;
+	default:
+		break;	
+}
 
 //------------------------------------------
 function isAnswer(q, event) {
